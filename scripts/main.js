6861e263396f3b0ae0cd053b0904327d9a36dd8b
Igor Boitsev <igor.boitsev@gmail.com>
2021-07-30 23:36:08 +0300

    single card

--- scripts/main.js
+++ scripts/main.js
@@ -17,10 +17,21 @@ const services = {
     alt: 'icon: avtoteka'
   },
   onlineBooking: {
-    title: 'Онлайн-ьронирование жилья',
+    title: 'Онлайн-бронирование жилья',
     text: 'Посуточная аренда квартир и домов: большой выбор вариантов для поездок по России',
     src: 'img/online-booking.svg',
     alt: 'icon: online-booking'
+  }
+}
+// Список рекламных блоков
+const commercials = {
+  yandex: {
+    title: 'Курс по рекламе в Instagram, FB, MT и TikTok',
+    src: 'img/yandex.jpg'
+  },
+  projector: {
+    title: 'Карманный проектор Atom Evolution',
+    src: 'img/atom.jpg'
   }
 }
 
@@ -39,9 +50,42 @@ const createPageElement = (tag, className, attributes) => {
   return element;
 }
 
+// Установка заголовка страницы
+document.title = 'Ну купи!';
+
 // Общая обертка для всех карточек товаров
 const cards = createPageElement('div',
                                ['cards']);
+
+// Функция получения данных с сервера
+const getData = async () => {
+  const data = await fetch('/db.json');
+
+  if (data.ok) {
+    return data.json();
+  } else {
+    throw new Error(`Данные не были получены. Ошибка ${data.status} ${data.statusText}`);
+  }
+}
+
+// Функция отрисовки всех карточек на странице
+const renderItemsList = (callback) => {
+  // Предварительная очистка содержимого
+  mainSection.textContent = '';
+  cards.textContent = '';
+  // Добавление заголовка для раздела страницы
+  mainSection.append(createPageElement('h2', ['page-title'], {textContent: 'Рекомендации для вас'}));
+  // console.log(mainSection.textContent);
+
+  getData()
+    .then(data => {
+      data.forEach(item => {
+        cards.append(callback(item));
+        mainSection.append(cards);
+      });
+    })
+    .catch(err => console.error(err));
+}
 
 // Функция создания одной карточки товара
 const createCardGood = (data) => {
@@ -98,31 +142,118 @@ const createCardGood = (data) => {
   card.append(cardImage, cardHeader, cardPrice, cardText, cardTextDate);
   return card;
 };
+// Функция создания раздела страницы с одним товаром
+const createSingleGood = (singleGood) => {
+  const mainSingleSection = createPageElement('div', ['main-single']);
 
-// Функция получения данных с сервера
-const getData = async () => {
-  const data = await fetch('/db.json');
+  // Обертка для фотогафий с описанием товара
+  const content = createPageElement('div', ['content']),
+        h2 = createPageElement('h2', ['page-title'], {textContent: singleGood.title});
 
-  if (data.ok) {
-    return data.json();
-  } else {
-    throw new Error(`Данные не были получены. Ошибка ${data.status} ${data.statusText}`);
+  content.append(h2);
+
+  // Добавление фотографий в слайдер
+  for (let i = 2; i >= 1; i--) {
+
+    const swiperContainer = createPageElement('div', ['swiper-container', 'mySwiper' + i]),
+          swiperWrapper = createPageElement('div', ['swiper-wrapper']);
+
+    singleGood.photo.forEach(src => {
+      const swiperSlide = createPageElement('div', ['swiper-slide']),
+            swiperSlideImage = createPageElement('img', ['swiper-slide-image'], {src: src, alt: singleGood.title});
+  
+      swiperSlide.append(swiperSlideImage);
+      swiperWrapper.append(swiperSlide);
+    });
+
+    swiperContainer.append(swiperWrapper);
+    content.append(swiperContainer);
   }
+
+  // Блок для описания товара
+  const contentText = createPageElement('div', ['content-text'], {textContent: singleGood.description});
+  content.append(contentText);
+
+  // Обертка для автора
+  const author = createPageElement('div', ['author']),
+        singlePrice = createPageElement('h2', ['single-price'], {textContent: `${singleGood.price} ₽`}),
+        authorWrapper = createPageElement('div', ['author-wrapper']),
+        authorInfo = createPageElement('div', ['author-info']),
+        authorName = createPageElement('span', ['author-name'], {textContent: singleGood.salesman}),
+        rating = createPageElement('div', ['rating']),
+        ratingCounter = createPageElement('span', ['rating-counter'], {textContent: `${singleGood.reviews.length} отзыва`})
+        authorStatus = createPageElement('span', ['author-status'], {textContent: singleGood.status}),
+        authorAvatar = createPageElement('img', ['author-avatar'], {src: singleGood.avatar, alt: singleGood.salesman});
+
+  // Добавление звездочек рейтинга
+  for (let i = 1; i <= singleGood.rating; i++) {
+    const filledStar = createPageElement('img', ['rating-star'], {src: 'img/filled-star.svg'});
+    rating.append(filledStar);
+  }
+  for (let j = 1; j <= (5 - singleGood.rating); j++) {
+    const unfilledStar = createPageElement('img', ['rating-star'], {src: 'img/unfilled-star.svg'});
+    rating.append(unfilledStar);
+  }
+
+  // Кнопки с номером телефона и сообщением
+  const buttonGroup = createPageElement('div', ['button-group']),
+        buttonPrimary = createPageElement('button',
+                                         ['button', 'button-block', 'button-primary'],
+                                         {
+                                           textContent: 'Показать телефон'
+                                         }),
+        buttonSuccess = createPageElement('button',
+                                         ['button', 'button-block', 'button-success'],
+                                         {
+                                           textContent: 'Написать сообщение'
+                                         }),
+        buttonBack = createPageElement('button',
+                                      ['button', 'button-block', 'button-back'],
+                                      {
+                                        textContent: 'Вернуться'
+                                      });
+  
+  buttonGroup.append(buttonPrimary, buttonSuccess, buttonBack)
+  
+  author.append(singlePrice);
+  rating.append(ratingCounter);
+  authorInfo.append(authorName, rating, authorStatus);
+  authorWrapper.append(authorInfo, authorAvatar);
+  author.append(authorWrapper, buttonGroup);
+
+  mainSingleSection.append(content, author);
+  mainSection.append(mainSingleSection);
+
+  // Активация слайдера
+  const swiper = new Swiper(".mySwiper1", {
+    spaceBetween: 10,
+    slidesPerView: 5,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  const swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    thumbs: {
+      swiper: swiper,
+    },
+  });
 }
 
-// Функция отрисовки всех карточек на странице
-const renderItemsList = (callback) => {
+// Функция отрисовки страницы для одного товара
+const renderSingleGood = (callback, id) => {
+  // Предварительная очистка содержимого
+  mainSection.textContent = '';
+
   getData()
     .then(data => {
-      data.forEach(item => {
-        cards.append(callback(item));
-        mainSection.append(cards);
-      });
+      const good = data.filter(item => item.id == id);
+      callback(...good);
     })
     .catch(err => console.error(err));
 }
 
-// Функцмя создания боковой колонки
+// Функция создания сервисов для боковой колонки
 const createServiceSidebar = (service) => {
   const serviceItem = createPageElement('div', ['services']);
   const srviceItemImage = createPageElement('img', 
@@ -151,18 +282,38 @@ const renderSevicesList = (services) => {
   servicesOrCommercial.insertAdjacentElement('afterbegin', sidebarTitle);
 }
 
+// Функция создания блоков рекламы для боковой колонки
+const createCommercialSidebar = (commercial) => {
+
+  const banner = createPageElement('a', ['banner'], {href: '#'}),
+        bannerImage = createPageElement('img', ['banner-image'], {src: commercial.src, alt: commercial.title});
+  banner.append(bannerImage);
+  servicesOrCommercial.insertAdjacentElement('afterbegin', banner);
+}
+// Отрисовка рекламы для сайдбара
+const renderCommercialList = (commercials) => {
+    // Предварительная очистка сайдбара
+  servicesOrCommercial.textContent = '';
+
+  for (const commercial in commercials)
+    createCommercialSidebar(commercials[commercial]);
+}
+
+// if (mainSection.textContent) console.log(1, mainSection.textContent);
 // Вывод карточек 
 renderItemsList(createCardGood);
 // Вывод боковой колонки сервисов и услуг
 renderSevicesList(services);
 
 
+
 mainSection.addEventListener('click', event => {
+  // Установка лайка или нелайка для товара
   if (event.target.closest('.heart')) {
 
     const elem = event.target.closest('.heart'),
-          card = event.target.closest('.card'),
-          elemId = event.target.closest('.card').id;
+          card = event.target.closest('.card');
+
     if (elem.alt == 'like') {
       card.classList.remove('liked');
       card.classList.add('unliked');
@@ -174,5 +325,26 @@ mainSection.addEventListener('click', event => {
       elem.alt = 'like';
       elem.src = 'img/like.svg';
     }
+  }
+
+  if (event.target.closest('.card-image') || event.target.closest('.card-title')) {
+    const elem = event.target.closest('.card'),
+          elemId = event.target.closest('.card').id,
+          elemTitle = elem.querySelector('.card-title').textContent;
+
+    // Установка заголовка страницы
+    document.title = elemTitle;
+
+    // Вызов функции вывода информации о выбранном товаре
+    renderSingleGood(createSingleGood, elemId);
+    renderCommercialList(commercials);
+  }
+})
+
+mainSection.addEventListener('click', event => {
+  // Возврат на главную страницы
+  if (event.target.closest('.button-back')) {
+    renderItemsList(createCardGood);
+    renderSevicesList(services);
   }
 })
